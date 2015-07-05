d8c25c15920c121fc022d840b02fcdaadb8464a3
jbn566 <jbn566@gmail.com>
2015-07-05 19:46:25 -0400

    Add Data Repo classes

--- CodeLibrary/BowtieLib/Application.cs
+++ CodeLibrary/BowtieLib/Application.cs
@@ -17,6 +17,7 @@ namespace JB2.Bowtie
 
         public Application(string publickey, string secretKey )
         {
+            
 
         }
 
---
+++ CodeLibrary/BowtieLib/ClientAPI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Bowtie.API.Client
+{
+    public static class BaseGame
+    {
+        private static Application _application;
+
+        public static bool InitilizeGame(string publicKey, string secret)
+        {
+            _application = new Application(publicKey, secret);
+
+            return true;
+
+        }
+    }
+}
