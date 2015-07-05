52f73eb1da127e2ecc88f558822cb7aae5aa839f
jbn566 <jbn566@gmail.com>
2015-07-05 17:03:01 -0400

    create basic Application class

--- CodeLibrary/BowtieLib/Application.cs
+++ CodeLibrary/BowtieLib/Application.cs
@@ -8,7 +8,53 @@ namespace JB2.Bowtie
 {
     public class Application : JB2.Common.IIDNamePair<string, string>
     {
+        private string _id;
+        private string _name;
+        private string _publickey;
+        private string _secret;
+
         
 
+        public Application(string publickey, string secretKey )
+        {
+
+        }
+
+        #region Public Properies
+
+        public string ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        
+        public string Secret
+        {
+            get
+            {
+                return _secret;
+            }
+        }
+
+       #endregion Public Properies
     }
 }
---
+++ CodeLibrary/BowtieLib/ApplicationManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Bowtie
+{
+    public static class ApplicationManager
+    {
+
+        public static string GetPublicKey(int applicationID)
+        {
+            switch(applicationID)
+            {
+                case 1:
+                    return "174B863C17";
+                default:
+                    return string.Empty;
+
+            }
+        }
+
+        public static string GetPublicKeyByPassPhrase(string phrase)
+        {
+            var key = JB2.Common.Utility.ComputeWep40(phrase);
+
+            var appKey = BitConverter.ToString(key[0]).Replace("-", "");
+
+            return appKey;
+        }
+
+    }
+}
