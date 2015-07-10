d803c8736a9a436e3dd0792e8be12b52ba6ae935
jbn566 <jbn566@gmail.com>
2015-07-09 22:06:28 -0400

    Fix issues of files on the azure published location

--- CodeLibrary/Bowtie.ConsoleTest/Program.cs
+++ CodeLibrary/Bowtie.ConsoleTest/Program.cs
@@ -43,7 +43,7 @@ namespace Bowtie.ConsoleTest
 
             Console.WriteLine("Calling the back-end API");
 
-            string apiBaseAddress = "http://jb2-bowtie.azurewebsites.net/"; //"http://localhost:52151/";
+            string apiBaseAddress = JB2.Bowtie.Settings.APIInfo.URL;
             //string apiBaseAddress = "http://localhost:52151/";
 
             BowtieDelegatingHandler customDelegatingHandler = new BowtieDelegatingHandler();
