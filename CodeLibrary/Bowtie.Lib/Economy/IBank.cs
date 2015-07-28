ec65cbad11015416377d84baec6a5ceda17329e4
jbn566 <jbn566@gmail.com>
2015-07-27 22:13:54 -0400

    Move economy stuff to Bowtie Main lib, Jbean a seperate project

---
+++ CodeLibrary/Bowtie.Lib/Economy/IBank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Bowtie
+{
+    
+    public interface IBank : IBank<ICurrency, long,string, byte, IDenomination<string>, byte, string>
+    {
+
+    }
+
+    public interface IBank<TCurrency,TCapital> :  JB2.Bowtie.IBank<TCurrency,TCapital,string,byte,IDenomination<string,string,byte,string,string>,byte,string>
+        where TCurrency : ICurrency<IDenomination<string,string, byte, string, string>,string>
+    {
+
+    }
+    public interface IBank<TCurrency,TCapital,TKey,TUnit,TDenomination,TMultiplier,TSerial>
+        where TCurrency : ICurrency<TKey,TUnit,TDenomination,string,TMultiplier,TSerial>
+        where TDenomination : JB2.Bowtie.IDenomination<string,TKey,TMultiplier,TKey,TSerial>
+    {
+        TCapital TotalCapital { get; set; }
+        float InterestRate { get; set; }
+
+       
+
+        int MaxAmountPerIssue { get; set; }
+
+    }
+}
---
+++ CodeLibrary/Bowtie.Lib/Economy/IWallet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Bowtie
+{
+    public interface IWallet<TOwner,TDenomination> : IWallet<TOwner,ulong,TDenomination,string,string>
+        where TDenomination : IDenomination<string,string, byte, string, string>
+    {
+
+    }
+    public interface IWallet<TOwner,TAmount,TDenomination, TKey,TDenominationType> : JB2.Common.IIDNamePair<TKey, string>
+        where TDenomination : IDenomination<TDenominationType, TKey, byte, TKey, string>
+    {
+        TOwner Owner { get; set; }
+        TDenomination[] Denomination { get;}
+        TAmount Amount { get; }
+
+        bool AddDenomination(TDenomination denomination,int quantity);
+        bool RemoveDenomination(TDenomination denomination, int quantity);
+    }
+
+}
