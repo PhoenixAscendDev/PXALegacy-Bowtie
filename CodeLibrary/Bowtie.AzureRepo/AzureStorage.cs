fe30cfce9b528f6d5dff410de5193d567c95ee56
Joshua Allen Bennett <jbn566@gmail.com>
2015-10-15 15:03:46 -0400

    Create Azure Repo lib.   GameobjectBlob

---
+++ CodeLibrary/Bowtie.AzureRepo/AzureStorage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.Storage;
+using JB2.Common.Data;
+
+namespace JB2.Bowtie.Data.Azure
+{
+    public static class AzureStorage
+    {
+
+        private static CloudStorageAccount _BowtieAccount
+        {
+            get
+            {
+                return JB2.Common.Data.AzureHelper.GetStorageAccount("jb2bowtie", "frIlemrNzlvAbKNhiyYCeW+otbFXBoJb0TodzbgwzF8IBEZMtifrHfx0Y+o1+jwvUL4FcAGYepHlgqrG0iCc1Q==");
+            }
+        }
+
+        private static CloudStorageAccount _JB2Account
+        {
+            get
+            {
+                return JB2.Common.Data.AzureHelper.GetStorageAccount("jbsquared", "iGf7AhI5v12hig85TsVkJSuPwvB42EncTMFogXFcqGlEcVMo5oXf0PvsMkxOQQeDmM21UtqHHtwyjR3MG3Di5g==");
+            }
+        }
+
+        public static AzureBlobRepository GameObjectsBlob
+        {
+            get
+            {
+                return new AzureBlobRepository(_BowtieAccount, "gameobjects");
+            }
+        }
+
+
+
+
+    }
+}
