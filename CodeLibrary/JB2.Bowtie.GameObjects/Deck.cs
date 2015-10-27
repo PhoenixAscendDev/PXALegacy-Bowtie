a63b4f330377e9e1d120866ad2f66d699599ebeb
Joshua Allen Bennett <jbn566@gmail.com>
2015-10-27 12:23:33 -0400

    Add concept of a Deck of items.  Able to pull from top, discard and reshuffle

---
+++ CodeLibrary/JB2.Bowtie.GameObjects/Deck.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JB2.Bowtie.Enum;
+using JB2.Common;
+
+namespace JB2.Bowtie.GameObjects
+{
+    public class Deck<Titem> : BowtieObject, IDeck<Titem>
+    {
+
+        #region Fields
+
+        protected List<Titem> _startqueue;
+
+        protected List<Titem> _queue;
+
+        protected List<Titem> _previous;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public Deck(string id, Titem[] items) : base(BowtieObjectType.bowtie_gameobject,id)
+        {
+            // due to List performance faster to add/remove at the bottom so lets reverse it so we pop the bottom
+            _startqueue = items.Reverse().ToList();
+            _queue = _startqueue;
+            _previous = new List<Titem>(items.Length);
+        }
+
+        private Deck(Titem[] items) : this(null,items)
+        {
+
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public Titem[] Stack
+        {
+            get
+            {
+                return (Titem[])_queue.ToArray().Reverse();
+            }
+        }
+
+        public Titem[] Discards
+        {
+            get
+            {
+                return _previous.ToArray();
+            }
+        }
+
+        public Titem Previous
+        {
+            get
+            {
+                return _previous.Last();
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _queue.Count;
+            }
+        }
+
+
+        public bool isEmpty
+        {
+            get
+            {
+                return _queue.Count == 0;
+            }
+        }
+
+        public GameObjectType GameObjectType
+        {
+            get
+            {
+                return GameObjectType.BingoBallQueue;
+            }
+        }
+
+        #endregion Properties
+
+        public Titem TakeTop()
+        {
+            Titem item = _queue.Last();
+
+            //now remove that ball from the queue and add it to the previous
+            _previous.Add(item);
+            _queue.RemoveAt(_queue.Count - 1);
+
+            return item;
+        }
+
+        public Titem Peek()
+        {
+            return _queue.Last();
+        }
+
+        public ServiceResult ResetQueue()
+        {
+            _queue = _startqueue;
+            _previous = new List<Titem>();
+
+            return true;
+        }
+
+    }
+}
---
+++ CodeLibrary/JB2.Bowtie.GameObjects/IDeck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Bowtie.GameObjects
+{
+    public interface IDeck<Tobject> : JB2.Bowtie.IGameObject
+    {
+        Tobject[] Stack { get; }
+
+        Tobject[] Discards { get; }
+
+        Tobject Previous { get; }
+
+        int Count{ get; }
+
+        bool isEmpty { get; }
+
+        Tobject TakeTop();
+
+        Tobject Peek();
+
+        JB2.Common.ServiceResult ResetQueue();
+
+    }
+}
