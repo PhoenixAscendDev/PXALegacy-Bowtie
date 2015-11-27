07115797b7a55e5b91253b4146232d434213debe
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-27 10:46:46 -0500

    Bowtie Core Lib and abstract Player class

--- CodeLibrary/Bowtie.Lib/Player.cs
+++ CodeLibrary/Bowtie.Lib/Player.cs
@@ -5,20 +5,76 @@ using System.Text;
 using System.Threading.Tasks;
 
 using JB2.Common;
-
+using JB2.Identity;
 
 namespace JB2.Bowtie
 {
-    public class Player
+    public abstract class Player  : JB2.Common.ShortGuidID, IBowtiePlayer
     {
         #region Fields
-        private JB2.Identity.IPlayer _player;
-                         
+        protected JB2.Identity.IPlayer _player;
+        protected Name _name;
+        protected JB2.Economy.IWallet _wallet;
+        protected MetaDataCollection _metadata;
+        protected Dictionary<string, MetaDataCollection> _modules;                
         #endregion Fields
 
-        public Player(JB2.Identity.IPlayer identityPlayer)
+
+        public abstract string DisplayName { get; set; }
+        public virtual Name Name
         {
-            _player = identityPlayer;
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
         }
+
+        #region IPlayer
+        public abstract int GetBitScore();
+        public abstract string GetFamilyID();
+        public abstract string GetjBeanAccountNumber();
+        public abstract string GetMasterEmail();
+        public abstract string GetMasterUsername();
+        public virtual IMetaData GetModuleMetaData(string module, string propertyName)
+        {
+            //MetaDataCollection collection = _modules[module];
+            if (_modules.ContainsKey(module))
+                return _modules[module][propertyName];
+            else
+                return null;
+        }
+
+        public abstract Name GetName();
+        public abstract IEnumerable<PlayerProfilePacket> GetProfiles();
+
+        public virtual IMetaData MetaData(string propertyName)
+        {
+            return _metadata[propertyName];
+        }
+        public virtual void SetModuleMetaData(string module, IMetaData metadata)
+        {
+            if (!_modules.ContainsKey(module))
+                _modules.Add(module, new MetaDataCollection(metadata));
+            else
+                _modules[module].Add(metadata);
+        }
+        #endregion IPlayer
+
+        #region IBowtiePlayer
+        public virtual JB2.Economy.IWallet GetWallet()
+        {
+            return _wallet;
+        }
+
+        public abstract string GetIdentityAuthID();
+
+        #endregion IBowtiePlayer
+
+
+
     }
 }
--- CodeLibrary/JB2.Bowtie.Core/IBowtiePlayer.cs
+++ CodeLibrary/JB2.Bowtie.Core/IBowtiePlayer.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JB2.Bowtie.Core
+namespace JB2.Bowtie
 {
     public interface IBowtiePlayer : JB2.Identity.IPlayer
     {
         string GetIdentityAuthID();
+
+        JB2.Economy.IWallet GetWallet();
         
     }
 }
