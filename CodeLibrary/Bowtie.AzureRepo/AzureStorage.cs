0c3916d43c2ffea290c0801863f13fe6c679b374
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-27 02:08:10 -0500

    Add JB2.Infrastucture to the Azure Repo

--- CodeLibrary/Bowtie.AzureRepo/AzureStorage.cs
+++ CodeLibrary/Bowtie.AzureRepo/AzureStorage.cs
@@ -12,27 +12,28 @@ namespace JB2.Bowtie.Data.Azure
     public static class AzureStorage
     {
 
-        private static CloudStorageAccount _BowtieAccount
-        {
-            get
-            {
-                return JB2.Common.Data.AzureHelper.GetStorageAccount("jb2bowtie", "frIlemrNzlvAbKNhiyYCeW+otbFXBoJb0TodzbgwzF8IBEZMtifrHfx0Y+o1+jwvUL4FcAGYepHlgqrG0iCc1Q==");
-            }
-        }
+        //private static CloudStorageAccount _BowtieAccount
+        //{
+        //    get
+        //    {
+        //        return JB2.Common.Data.AzureHelper.GetStorageAccount("jb2bowtie", "frIlemrNzlvAbKNhiyYCeW+otbFXBoJb0TodzbgwzF8IBEZMtifrHfx0Y+o1+jwvUL4FcAGYepHlgqrG0iCc1Q==");
+        //    }
+        //}
 
-        private static CloudStorageAccount _JB2Account
-        {
-            get
-            {
-                return JB2.Common.Data.AzureHelper.GetStorageAccount("jbsquared", "iGf7AhI5v12hig85TsVkJSuPwvB42EncTMFogXFcqGlEcVMo5oXf0PvsMkxOQQeDmM21UtqHHtwyjR3MG3Di5g==");
-            }
-        }
+        //private static CloudStorageAccount _JB2Account
+        //{
+        //    get
+        //    {
+        //        return JB2.Common.Data.AzureHelper.GetStorageAccount("jbsquared", "iGf7AhI5v12hig85TsVkJSuPwvB42EncTMFogXFcqGlEcVMo5oXf0PvsMkxOQQeDmM21UtqHHtwyjR3MG3Di5g==");
+        //    }
+        //}
 
         public static AzureBlobRepository GameObjectsBlob
         {
             get
             {
-                return new AzureBlobRepository(_BowtieAccount, "gameobjects");
+                return JB2.Infrastructure.Storage.BowtieAccount.GetBlog("gameobjects");
+                //return new AzureBlobRepository(_BowtieAccount, "gameobjects");
             }
         }
 
@@ -40,7 +41,8 @@ namespace JB2.Bowtie.Data.Azure
         {
             get
             {
-                return new AzureTableRepository(_BowtieAccount, "gameobjects");
+                return JB2.Infrastructure.Storage.BowtieAccount.GetTable("gameobjects");
+                //return new AzureTableRepository(_BowtieAccount, "gameobjects");
             }
         }
 
