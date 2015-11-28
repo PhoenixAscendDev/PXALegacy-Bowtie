f3c45d944bc6f6b323b7cf6240eafc9f358671c4
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-27 20:34:12 -0500

    add reference to JB2 Infrastructure

--- CodeLibrary/Bowtie.AzureRepo/AzureStorage.cs
+++ CodeLibrary/Bowtie.AzureRepo/AzureStorage.cs
@@ -12,21 +12,6 @@ namespace JB2.Bowtie.Data.Azure
     public static class AzureStorage
     {
 
-        //private static CloudStorageAccount _BowtieAccount
-        //{
-        //    get
-        //    {
-        //        return JB2.Common.Data.AzureHelper.GetStorageAccount("jb2bowtie", "frIlemrNzlvAbKNhiyYCeW+otbFXBoJb0TodzbgwzF8IBEZMtifrHfx0Y+o1+jwvUL4FcAGYepHlgqrG0iCc1Q==");
-        //    }
-        //}
-
-        //private static CloudStorageAccount _JB2Account
-        //{
-        //    get
-        //    {
-        //        return JB2.Common.Data.AzureHelper.GetStorageAccount("jbsquared", "iGf7AhI5v12hig85TsVkJSuPwvB42EncTMFogXFcqGlEcVMo5oXf0PvsMkxOQQeDmM21UtqHHtwyjR3MG3Di5g==");
-        //    }
-        //}
 
         public static AzureBlobRepository GameObjectsBlob
         {
