b801c3b2b29a070a485902be5ea0f0f3bfe178c7
Joshua Allen Bennett <jbn566@gmail.com>
2016-02-13 16:01:44 -0500

    Add GraphService to the Bowtie Service lib

---
+++ CodeLibrary/Bowtie.Service/GraphService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Bowtie.Service
+{
+    public class GraphService
+    {
+        #region Fields
+        protected IGraphRepository  _repo;
+        protected IUnitOfWork _uofw;
+        #endregion Fields
+
+        #region Constructors
+        public GraphService(IUnitOfWork uofw)
+        {
+            _uofw = uofw;
+            _repo = uofw.GraphRepository;
+        }
+
+        public GraphService(IGraphRepository repo)
+        {
+            _repo = repo;
+            _uofw = JB2.Settings.Bowtie.UnitOfWork;
+        }
+
+        #endregion Constructors
+
+        public IEnumerable<GraphStory> RetreiveStoriesByApplication(Application app)
+        {
+            var elements = _repo.GetGraphElementsByApplication(app.ID);
+
+            return filterByType<GraphStory>(elements, Enum.GraphElementType.Story);          
+        }
+
+
+        #region Graph Properties
+        public IEnumerable<GraphProperty> RetreiveProperties()
+        {
+            var elements = _repo.GetGraphElementsByType(Enum.GraphElementType.Property);
+
+            return filterByType<GraphProperty>(elements, Enum.GraphElementType.Property);
+        }
+
+        public IEnumerable<GraphProperty> RetreivePropertiesByApplication(Application app)
+        {
+            var elements = _repo.GetGraphElementsByApplication(app.ID);
+            return filterByType<GraphProperty>(elements, Enum.GraphElementType.Property);
+        }
+
+        public void SaveProperty(GraphProperty prop)
+        {
+            _repo.InsertGraphElement(prop);
+        }
+        #endregion Graph Properties
+
+        #region internal helpers
+        protected IEnumerable<T> filterByType<T>(IEnumerable<IGraphElement> list,Enum.GraphElementType type)
+            where T : IGraphElement
+        {
+            List<T> result = new List<T>(list.Count());
+
+            foreach (IGraphElement e in list)
+            {
+                if (e.ElementType == type)
+                    result.Add((T)e);
+            }
+
+            return result;
+        }
+
+        #endregion internal helpers
+
+    }
+}
