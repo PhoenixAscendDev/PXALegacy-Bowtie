9d0c5cae684fb27d4719592bcb3eec539b720a47
jbn566 <jbn566@gmail.com>
2016-02-26 00:54:35 -0500

    Add Javascript SDK

---
+++ Websites/Bowtie.WebSDK/sdk.js
@@ -0,0 +1,20 @@
+﻿(function(window, undefined) {
+    var Bowtie = {};
+
+    if (window.Bowtie)
+        return;
+
+    Bowtie.init = function(callback) {
+        Bowtie.script("{{ service_url_for('example', chapter='08', name='web-service-api', file='lib.js') }}", function () {
+            Bowtie._initializeLibrary(callback);
+        });
+    };
+
+    window.Bowtie = Bowtie;
+})(this);
+
+// Embed the 'script' microlib into our initial script file for loading dependencies.
+
+{% include 'script.js' %}
+
+Bowtie.script = $script.noConflict();
