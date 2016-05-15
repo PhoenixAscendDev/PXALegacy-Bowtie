98130f03108905abdd5d4a158c2b0e8b0726dc67
Joshua Allen Bennett <jbn566@gmail.com>
2016-05-15 10:26:19 -0400

    Add ApplicationPlayer to Bowtie

--- CodeLibrary/Bowtie.Service/PlayerService.cs
+++ CodeLibrary/Bowtie.Service/PlayerService.cs
@@ -8,5 +8,19 @@ namespace JB2.Bowtie.Service
 {
     public class PlayerService : GenericService<IBowtiePlayer, IBowtiePlayerRespository>
     {
+        public IBowtiePlayer RetrieveByAuthID(string id,IApplication app)
+        {
+            var playerid = JB2.Identity.PlayerStore.GetClientPlayerID(id, app.ClientID);
+
+            var appPlayer = JB2.Identity.PlayerStore.GetPlayerByAppPlayerID(playerid, app.GetID());
+
+            IBowtiePlayer result = new JB2.Bowtie.ApplicationPlayer(appPlayer, app.GetID());
+
+            _uofw.PlayerRepository.Insert(result);
+
+            return result;
+
+
+        }
     }
 }
---
+++ CodeLibrary/JB2.Bowtie.Core/AppPlayer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JB2.Common;
+using JB2.Identity;
+
+namespace JB2.Bowtie
+{
+    public class ApplicationPlayer : Player, JB2.Identity.IApplicationable<string>
+    {
+        #region Fields
+
+        protected string _applicationID;
+
+
+        #endregion Fields
+
+
+        #region Constructors
+        public ApplicationPlayer(JB2.Identity.IPlayer authPlayer, string applicationID)
+        {
+            _player = authPlayer;
+            _applicationID = applicationID;
+        }
+
+        public override string DisplayName
+        {
+            get
+            {
+                return _player.DisplayName;
+            }
+
+            set
+            {
+                _player.DisplayName = value;
+            }
+        }
+
+        #endregion Constructors
+
+        #region IApplicationable
+        public string GetApplicationID()
+        {
+            return _applicationID;
+        }
+
+
+
+        #endregion IApplicationable
+
+        #region IBowtiePlayer
+
+        public override int GetBitScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetFamilyID()
+        {
+            return _player.GetFamilyID();
+        }
+
+        public override string GetIdentityAuthID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetjBeanAccountNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetMasterEmail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetMasterUsername()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Name GetName()
+        {
+            return _player.Name;
+        }
+
+        public override IEnumerable<PlayerProfilePacket> GetProfiles()
+        {
+            var packet = this._player.GetDefaultProfile();
+            return new PlayerProfilePacket[1] { packet };
+        }
+
+
+        #endregion IBowtiePlayer
+
+
+
+    }
+}
