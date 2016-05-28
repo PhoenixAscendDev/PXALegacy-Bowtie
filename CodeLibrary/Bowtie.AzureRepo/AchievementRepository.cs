a3099f2b4296689027354041fc9d5193547d64e2
Joshua Allen Bennett <jbn566@gmail.com>
2016-05-28 13:54:08 -0400

    Define the logic for the AchievementRepository in Azure Storage

--- CodeLibrary/Bowtie.AzureRepo/AchievementRepository.cs
+++ CodeLibrary/Bowtie.AzureRepo/AchievementRepository.cs
@@ -6,23 +6,37 @@ using System.Threading.Tasks;
 
 using Microsoft.WindowsAzure.Storage.Table;
 
+using JB2.Common;
+using JB2.Common.Data;
+using JB2.Identity;
+
 namespace JB2.Bowtie.Data.Azure
 {
     public class AchievementRepository : BowtieRepository<IAchievement>, IAchievementRepository
     {
+        #region Constructors
+        public AchievementRepository() : this("achievements","general")
+        {
 
+        }
+
+        protected AchievementRepository(string tableName, string blobName)
+            : this(JB2.Infrastructure.Storage.BowtieAccount.GetTable(tableName),
+                   JB2.Infrastructure.Storage.BowtieAccount.GetBlog(blobName))
+        {
+
+        }
+
+        public AchievementRepository(AzureTableRepository azureTable, AzureBlobRepository azureBlob) : base()
+        {
+            _table = azureTable;
+            _blob = azureBlob;
+            _defaultPartitionKey = "achievement";
+        }
+
+        #endregion
 
         public IAchievement[] GetAchievementsByApplication(string appID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override IAchievement[] GetAll(bool useCache = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override IAchievement GetById(string id, bool useCache = true)
         {
             throw new NotImplementedException();
         }
@@ -49,17 +63,91 @@ namespace JB2.Bowtie.Data.Azure
 
         protected override DynamicTableEntity convertToEntity(IAchievement o)
         {
-            throw new NotImplementedException();
+            var e = new DynamicTableEntity();
+
+            e.Properties.Add("ID", new EntityProperty(o.GetID()));
+            e.Properties.Add("Kind", new EntityProperty(o.GetKind().ToString()));
+            e.Properties.Add("LastUpdate", new EntityProperty(o.GetLastUpdate()));
+            e.Properties.Add("Name", new EntityProperty(o.GetName()));
+            e.Properties.Add("AchievementType", new EntityProperty(o.AchievementType.ToString());
+            e.Properties.Add("ApplicationID", new EntityProperty(o.ApplicationID));
+            e.Properties.Add("Category", new EntityProperty(o.Category));
+            e.Properties.Add("Description", new EntityProperty(o.Description));
+            e.Properties.Add("DewdropCSV", new EntityProperty(string.Join(",", o.DewdropTriggers.ToArray())));
+            e.Properties.Add("EarnedIconUrl", new EntityProperty(o.EarnedIconUrl));
+            e.Properties.Add("HiddenIconUrl", new EntityProperty(o.HiddenIconUrl));
+            e.Properties.Add("ShownIconUrl", new EntityProperty(o.ShownIconUrl));
+            e.Properties.Add("Points", new EntityProperty(o.Points));
+            e.Properties.Add("Rarity", new EntityProperty(o.Rarity.ToString()));
+            e.Properties.Add("StepRegEx", new EntityProperty(o.StepRegex));
+            e.Properties.Add("StepRequired", new EntityProperty(o.StepsRequired));
+            e.Properties.Add("TimeBoundEnd", new EntityProperty(o.TimeBoundEnd));
+            e.Properties.Add("TimeBoundStart", new EntityProperty(o.TimeBoundStart));
+            e.Properties.Add("UniqueToken", new EntityProperty(o.UniqueToken));
+            e.Properties.Add("SortOrder", new EntityProperty(o.SortOrder));
+
+            e.PartitionKey = _defaultPartitionKey;
+            e.RowKey = "id:" + o.GetID();
+
+            return e;       
         }
 
         protected override IEnumerable<IAchievement> convertToObject(IEnumerable<DynamicTableEntity> list)
         {
-            throw new NotImplementedException();
+            var result = new List<IAchievement>();
+
+            foreach(var e in list)
+            {
+                result.Add(convertToObject(e));
+            }
+
+            return result;
         }
 
         protected override IAchievement convertToObject(DynamicTableEntity e)
         {
-            throw new NotImplementedException();
+            IAchievement result = null;
+
+            Enum.AchievementType type = e.Properties.ContainsKey("AchievementType") ? (Enum.AchievementType)System.Enum.Parse(typeof(Enum.AchievementType), e.Properties["AchievementType"].StringValue) : Enum.AchievementType.Standard;
+            string id = e.Properties.ContainsKey("ID") ? e.Properties["ID"].StringValue : string.Empty;
+
+            switch (type)
+            {
+                case Enum.AchievementType.TimeBound:
+                    result = new EventAchievement(id);
+                    break;
+                default :
+                    result = new StandardAchievement(id);
+                    break;
+            }
+
+            result.Name = e.Properties["Name"].StringValue;
+            result.AchievementType = type;
+            result.ApplicationID = e.Properties["ApplicationID"].StringValue;
+            result.Category = e.Properties["Category"].StringValue;
+            result.Description = e.Properties["Description"].StringValue;
+
+            var drewdroptriggers = e.Properties["DewdropCSV"].StringValue.Split(',');
+            result.DewdropTriggers = drewdroptriggers;
+
+            result.EarnedIconUrl = e.Properties["EarnedIconUrl"].StringValue;
+            result.HiddenIconUrl = e.Properties["HiddenIconUrl"].StringValue;
+            result.ShownIconUrl = e.Properties["ShownIconUrl"].StringValue;
+            result.Points = (long)e.Properties["Points"].Int64Value;
+
+            result.Rarity = e.Properties.ContainsKey("Rarity") ? (Enum.AchievementRarityType)System.Enum.Parse(typeof(Enum.AchievementRarityType), e.Properties["Rarity"].StringValue) : Enum.AchievementRarityType.Common;
+
+            result.StepRegex = e.Properties["StepRegEx"].StringValue;
+            result.StepsRequired = e.Properties["StepRequired"].Int32Value.GetValueOrDefault();
+
+            result.TimeBoundEnd = e.Properties["TimeBoundEnd"].DateTime.GetValueOrDefault();
+            result.TimeBoundStart = e.Properties["TimeBoundStart"].DateTime.GetValueOrDefault();
+
+            //result.UniqueToken = e.Properties["UniqueToken"].StringValue;
+
+            
+
+            return result;
         }
 
         protected override void deleteAll(DynamicTableEntity e)
@@ -69,7 +157,41 @@ namespace JB2.Bowtie.Data.Azure
 
         protected override void saveEntity(DynamicTableEntity e, bool replace)
         {
-            throw new NotImplementedException();
+            //first delete any records associated with partiion of the id
+            e.PartitionKey = _defaultPartitionKey + ":" + e.Properties["ID"].StringValue;
+            var marktoDelete = _table.GetByPartitionKey<DynamicTableEntity>(e.PartitionKey, 1000);
+            foreach (var d in marktoDelete)
+            {
+                _table.Delete<DynamicTableEntity>(d.PartitionKey, d.RowKey);
+            }
+
+
+
+            //standard by ID
+            e.PartitionKey = _defaultPartitionKey;
+            e.RowKey = "id:" + e.Properties["ID"].StringValue;
+            _table.Insert<DynamicTableEntity>(e, TableInsertMode.Merge, false);
+
+            //by Application
+            e.PartitionKey = _defaultPartitionKey + ":app:" + e.Properties["ApplicationID"].StringValue;
+            e.RowKey = "id:" + e.Properties["ID"].StringValue;
+            _table.Insert<DynamicTableEntity>(e, TableInsertMode.Merge, false);
+
+            e.PartitionKey = _defaultPartitionKey + ":type:" + e.Properties["AchievementType"].StringValue;
+            e.RowKey = "id:" + e.Properties["ID"].StringValue;
+            _table.Insert<DynamicTableEntity>(e, TableInsertMode.Merge, false);
+
+            //by dewdrops
+            List<string> dewdrops = e.Properties["DewdropCSV"].StringValue.Split(',').ToList();
+            e.PartitionKey = _defaultPartitionKey + ":" + e.Properties["ID"].StringValue;
+            foreach( string dewdrop in dewdrops)
+            {
+                e.RowKey  = "dewdrop:" + dewdrop + "_id:" + e.Properties["ID"].StringValue;
+                _table.Insert<DynamicTableEntity>(e, TableInsertMode.Merge, false);
+            }
+
+
+
         }
     }
 }
