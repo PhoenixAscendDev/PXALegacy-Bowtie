120064fb3bb52bcbd536cae53907748a207f261e
Joshua Allen Bennett <jbn566@gmail.com>
2016-05-31 07:52:54 -0400

    laying out the general theme layout

---
+++ Websites/Bowtie.WebMain/Controllers/ApplicationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JB2.Bowtie.Web.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        // GET: Application
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
