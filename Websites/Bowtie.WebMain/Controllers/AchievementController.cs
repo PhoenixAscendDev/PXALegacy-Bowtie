44c105d2c91cfbd3e2aabdba6ec424acf3dc7446
Joshua Allen Bennett <jbn566@gmail.com>
2016-06-05 03:21:16 -0400

    Add Achievement new page

---
+++ Websites/Bowtie.WebMain/Controllers/AchievementController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using JB2.Bowtie.Web.Models;
+
+namespace JB2.Bowtie.Web.Controllers
+{
+    public class AchievementController : BaseController
+    {
+        // GET: Achievement
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            ViewBag.Applications = this.applicationSelectList();
+            ViewBag.Achievements = AchievementService.Retrieve();
+            AchievementViewModel model = new AchievementViewModel();
+            return View(model);
+        }
+    }
+}
---
+++ Websites/Bowtie.WebMain/Models/AchievementViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JB2.Bowtie.Web.Models
+{
+    public class AchievementViewModel
+    {
+        public string ApplicationId {get; set;}
+        public string Description { get; set; }
+        public int StepsRequired { get; set; }
+        public int PointsEarned { get; set; }
+
+        public string AchievementType { get; set; }
+        public DateTime EventDateEndStart { get; set; }
+        public DateTime EventDateEnd { get; set; }
+
+
+    }
+}
