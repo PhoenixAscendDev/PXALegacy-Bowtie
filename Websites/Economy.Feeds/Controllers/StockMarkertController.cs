dc0234448b37644a6e50b9255ca8a3063a296062
Joshua Allen Bennett <jbn566@gmail.com>
2016-10-07 04:03:32 -0400

    Add packages to Economy Feed

---
+++ Websites/Economy.Feeds/Controllers/StockMarkertController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Economy.Feeds.Controllers
+{
+    public class StockMarkertController : Controller
+    {
+        // GET: StockMarkert
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
