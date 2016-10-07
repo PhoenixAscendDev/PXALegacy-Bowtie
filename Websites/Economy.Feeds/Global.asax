684d0ce9ab439ff2d955835aeb2d9e9c4fdd7848
Joshua Allen Bennett <jbn566@gmail.com>
2016-10-07 03:44:32 -0400

    New Webiste - Economy Feeds (RSS)

---
+++ Websites/Economy.Feeds/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Economy.Feeds.MvcApplication" Language="C#" %>
---
+++ Websites/Economy.WebAPI/Controllers/StockMarketController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+using JB2.Common.WebAPI;
+using JB2.Identity;
+
+using JB2.Common;
+
+namespace JB2.Economy.WebAPI.Controllers
+{
+    public class StockMarketController : BaseApiController
+    {
+
+    }
+}
