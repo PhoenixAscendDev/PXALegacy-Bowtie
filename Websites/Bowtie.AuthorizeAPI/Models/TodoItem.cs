ad69f569a8067d518d8abbe0c35778a7b7aeb0d8
Joshua Allen Bennett <jbn566@gmail.com>
2016-10-24 19:00:44 -0400

    Add Authorize API site

---
+++ Websites/Bowtie.AuthorizeAPI/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bowtie.AuthorizeAPI.Models
+{
+    public class TodoItem
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
