f0c41e65a4b7c9554ce367f29d65e0fe36e48af7
Joshua Allen Bennett <jbn566@gmail.com>
2016-10-25 01:21:21 -0400

    Test Authorize API results

---
+++ Websites/Bowtie.AuthorizeAPI/Controllers/AppAuthorizeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Bowtie.AuthorizeAPI.Models;
+using JB2.Common;
+using JB2.Bowtie;
+
+namespace Bowtie.AuthorizeAPI.Controllers
+{
+    [RoutePrefix("api/Application")]
+    public class AppAuthorizeController : JB2.Common.WebAPI.BaseApiController
+    {
+
+        public AppAuthorizeController() : base()
+        {
+
+        }
+
+
+        [HttpPost]
+        public HttpResponseMessage CheckAuthorize(string publicKey, string secret)
+        {
+            IUnitOfWork uofw = new JB2.Bowtie.Data.Azure.UnitOfWork();
+            var appService = new JB2.Bowtie.Service.ApplicationService(uofw);
+
+            ApplicationStatePair appState = appService.RetrieveAuthorizeState(publicKey, secret);
+
+            ServiceResult isAuth = appService.isAuthorized(appState);
+
+            ApplicationStateView result = new ApplicationStateView();
+            var authkeyURL = "http://bowtieAuth.io?key=" + publicKey + "&secret=" + secret;
+
+
+            result.ApplicationID = appState.ApplicationID;
+            result.AuthorizeKey = JB2.Common.NewID.UriHash(new Uri(authkeyURL));
+            result.isAuthorized = isAuth;
+            result.Message = isAuth.ToString();
+            result.State = appState.ToString();
+
+            result.SetSerializableProperties(null);
+
+            return this.createResponse<ApplicationStateView>(result, "all", HttpStatusCode.OK);
+
+        }
+    }
+}
---
+++ Websites/Bowtie.AuthorizeAPI/Models/ApplicationStateResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using JB2.Common;
+using JB2.Common.WebAPI;
+
+namespace Bowtie.AuthorizeAPI.Models
+{
+    public class ApplicationStateView : APIObject
+    {
+
+        public string ApplicationID { get; set; }
+        public string State { get; set; }
+
+        public string Message { get; set; }
+
+        public bool isAuthorized { get; set; }
+
+        public string AuthorizeKey { get; set; }
+    }
+}
