5f96ad479a41a47489c7393488ac016ab316d037
Joshua Allen Bennett <jbn566@gmail.com>
2016-12-06 18:27:02 -0500

    Add IClassConfig and ClassConfig

---
+++ CodeLibrary/JB2.Bowtie.Core/ClassConfig.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JB2.Common.Enum;
+
+using JB2.Common;
+
+namespace JB2.Common
+{
+    public class ClassConfig : JB2.Common.JB2Class, IClassConfig
+    {
+        public ClassConfig()
+        {
+            _props = new MetaDataCollection();
+        }
+
+        public string Assembly
+        {
+            get
+            {
+                return _props.GetProperty<string>("Assembly");
+            }
+
+            set
+            {
+                _props.SetProperty<string>("Assembly",value);
+            }
+        }
+
+        public string AssemblyQualifiedName
+        {
+            get
+            {
+                return _props.GetProperty<string>("AssemblyQualifiedName");
+            }
+
+            set
+            {
+                _props.SetProperty<string>("AssemblyQualifiedName", value);
+            }
+        }
+
+        public string Classname
+        {
+            get
+            {
+                return _props.GetProperty<string>("Classname");
+            }
+
+            set
+            {
+                _props.SetProperty<string>("Classname", value);
+            }
+        }
+
+        public ClassType ClassType
+        {
+            get
+            {
+                return _props.GetProperty<ClassType>("ClassType");
+            }
+
+            set
+            {
+                _props.SetProperty<ClassType>("ClassType", value);
+            }
+        }
+
+        public string[] ConstructorParameters
+        {
+            get
+            {
+                return _props.GetProperty<string[]>("ConstructorParameters");
+            }
+
+            set
+            {
+                _props.SetProperty<string[]>("ConstructorParameters", value);
+            }
+        }
+
+        public string Namespace
+        {
+            get
+            {
+                return _props.GetProperty<string>("Namespace");
+            }
+
+            set
+            {
+                _props.SetProperty<string>("Namespace", value);
+            }
+        }
+
+        public int GetConstructorParameterCount()
+        {
+            return ConstructorParameters.Count();
+        }
+    }
+}
---
+++ CodeLibrary/JB2.Bowtie.Core/ObjectConfig.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JB2.Common.Enum;
+
+namespace JB2.Bowtie
+{
+    public abstract class ObjectConfig : JB2.Common.IClassConfig
+    {
+
+        #region Fields
+        protected JB2.Common.ClassConfig _classconfig;
+        #endregion Fields
+
+        #region Constructors
+        public ObjectConfig()
+        {
+            _classconfig = new Common.ClassConfig();
+        }
+
+
+        #endregion Constructors
+
+        #region IClassConfig
+
+        public string Assembly
+        {
+            get
+            {
+                return _classconfig.Assembly;
+            }
+
+            set
+            {
+                _classconfig.Assembly = value;
+            }
+        }
+
+        public string AssemblyQualifiedName
+        {
+            get
+            {
+                return _classconfig.AssemblyQualifiedName
+            }
+
+            set
+            {
+                _classconfig.AssemblyQualifiedName = value;
+            }
+        }
+
+        public string Classname
+        {
+            get
+            {
+                return _classconfig.Classname;
+            }
+
+            set
+            {
+                _classconfig.Classname = value;
+            }
+        }
+
+        public ClassType ClassType
+        {
+            get
+            {
+                return _classconfig.ClassType;
+            }
+
+            set
+            {
+                _classconfig.ClassType = value;
+            }
+        }
+
+        public string[] ConstructorParameters
+        {
+            get
+            {
+                return _classconfig.ConstructorParameters;
+            }
+
+            set
+            {
+                _classconfig.ConstructorParameters = value;
+            }
+        }
+
+        public string Namespace
+        {
+            get
+            {
+                return _classconfig.Namespace;
+            }
+
+            set
+            {
+                _classconfig.Namespace = value;
+            }
+        }
+
+        public int GetConstructorParameterCount()
+        {
+            return _classconfig.GetConstructorParameterCount();
+        }
+
+
+        #endregion IClassConfig
+
+
+    }
+}
