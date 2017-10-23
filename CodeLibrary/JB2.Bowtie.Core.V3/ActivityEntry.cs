f4aeb3765c528ff584412886cc32e25ff5b24ade
Joshua Allen Bennett <jbn566@gmail.com>
2017-10-23 15:37:52 -0400

    Add ActivityEntry class, Core version 3.9

---
+++ CodeLibrary/JB2.Bowtie.Core.V3/ActivityEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Bowtie
+{
+    public class ActivityEntry : Bowtie.BowtieObject, IActivityEntry
+    {
+
+        public string ApplicationID { get; set; }
+
+        public string PlayerID { get; set; }
+        public DateTime ActivityDate { get; set; }
+        public ActivityDataset Data { get; set; }
+        public string ActivityCode { get; set; }
+
+        public string GetApplicationID()
+        {
+            return ApplicationID;
+        }
+
+        public string GetPlayerID()
+        {
+            return PlayerID;
+        }
+
+        public static ActivityEntry New
+        {
+            get
+            {
+                var r = new ActivityEntry();
+                r.ID = JB2.Common.NewID.Guid();
+                return r;
+            }
+        }
+    }
+}
